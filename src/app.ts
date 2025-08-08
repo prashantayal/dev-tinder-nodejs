d4c59e37cadff7b6c6ff00897206813a982cf768
prashanttayal <prash92ant@gmail.com>
2025-08-08 13:06:16 +0530

    exploring GET POST DELETE

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,26 @@ const express = require("express");
 
 const app = express();
 
+// MAtch all the HTTP method API calls to '/test'
 app.use("/test", (req: any, res: any) => {
   res.send("Hey! 👋");
 });
 
-app.listen(3000, () => {
+// GET API call - only handle GET call to '/user'
+app.get("/user", (req: any, res: any) => {
+  res.send("User data will be returned here");
+});
+
+// POST API call - only handle POST call to '/user'
+app.post("/user", (req: any, res: any) => {
+  res.send("User data save successfully");
+});
+
+// DELETE API call
+app.delete("/user", (req: any, res: any) => {
+  res.send("User data deleted successfully");
+});
+
+app.listen(8000, () => {
   console.log("Server is up and running...");
 });
